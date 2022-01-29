6b09fc98738434cc0d65dbe63bbaf72151905890
askar <askar@sosiv.io>
2022-01-29 17:03:22 +0600

    Added create and remove user functionalities

--- src/repositories/users/UserRepository.java
+++ src/repositories/users/UserRepository.java
@@ -58,4 +58,35 @@ public class UserRepository implements EntityRepository<User> {
 
         return users;
     }
+
+    @Override
+    public boolean create(User user) {
+        Connection conn = db.getConnection();
+
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute("INSERT INTO users(name, surname) " +
+                    "VALUES('" + user.getName() + "','" + user.getSurname() + "')");
+
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Connection conn = db.getConnection();
+
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute("DELETE FROM users WHERE id = " + id);
+
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
 }
