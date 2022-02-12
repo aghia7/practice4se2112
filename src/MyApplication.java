2b77ba1396331244948c9dea90720fe25a49b8f5
askar <askar@sosiv.io>
2022-02-13 01:14:41 +0600

    Finished products

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -24,32 +24,24 @@ public class MyApplication {
 
             int option = sc.nextInt();
 
-            if (option == 1)
-                usersMenu();
-            else if (option == 2)
-                productMenu();
-            else
-                break;
+            try {
+                if (option == 1)
+                    usersMenu();
+                else if (option == 2)
+                    productMenu();
+                else
+                    break;
+            } catch (Exception e) {
+                System.out.println("Something went wrong: Error " + e.getMessage());
+                System.out.println("0. Enter any symbol to return to main menu...");
+                sc.next();
+            }
         }
     }
 
-    private void productMenu() {
-        while (true) {
-            System.out.println("+++++++++++++++++++++++++++++");
-            System.out.println("Product`s menu");
-            System.out.println("Please, select option:");
-            System.out.println("1. Get all products by category");
-            System.out.println("0. Exit from product`s menu");
 
-            int option = sc.nextInt();
 
-            if (option == 1)
-                getAllProductsByCategory();
-            else
-                break;
-        }
-    }
-
+    // Users START
     private void usersMenu() {
         while (true) {
             System.out.println("+++++++++++++++++++++++++++++");
@@ -74,16 +66,6 @@ public class MyApplication {
             else
                 break;
         }
-    }
-
-    private void getAllProductsByCategory() {
-        System.out.println("Enter category: ");
-        String category = sc.next();
-        String resp = productCtrl.getAllByCategory(category);
-        System.out.println(resp);
-
-        System.out.println("0. Enter any symbol to exit...");
-        sc.next();
     }
 
     private void removeUserById() {
@@ -125,4 +107,91 @@ public class MyApplication {
         System.out.println("0. Enter any symbol to exit...");
         sc.next();
     }
+
+    // Users END
+
+    // Products START
+    private void productMenu() {
+        while (true) {
+            System.out.println("+++++++++++++++++++++++++++++");
+            System.out.println("Product`s menu");
+            System.out.println("Please, select option:");
+            System.out.println("1. Create product");
+            System.out.println("2. Get product by id");
+            System.out.println("3. Get all products");
+            System.out.println("4. Remove product by id");
+            System.out.println("5. Get all products by category");
+            System.out.println("0. Exit from product`s menu");
+
+            int option = sc.nextInt();
+
+            if (option == 1)
+                createProduct();
+            else if (option == 2)
+                getProductById();
+            else if (option == 3)
+                getAllProducts();
+            else if (option == 4)
+                removeProductById();
+            else if (option == 5)
+                getAllProductsByCategory();
+            else
+                break;
+        }
+    }
+
+    private void removeProductById() {
+        System.out.println("Enter id: ");
+        int id = sc.nextInt();
+        String resp = productCtrl.removeById(id);
+        System.out.println(resp);
+
+        System.out.println("0. Enter any symbol to exit...");
+        sc.next();
+    }
+
+    private void getAllProducts() {
+        String resp = productCtrl.getAll();
+        System.out.println(resp);
+
+        System.out.println("0. Enter any symbol to exit...");
+        sc.next();
+    }
+
+    private void getProductById() {
+        System.out.println("Enter id: ");
+        int id = sc.nextInt();
+        String resp = productCtrl.getById(id);
+        System.out.println(resp);
+
+        System.out.println("0. Enter any symbol to exit...");
+        sc.next();
+    }
+
+    private void createProduct() {
+        System.out.println("Enter name:");
+        sc.nextLine(); // for ignoring unwanted empty string
+        String name = sc.nextLine();
+        System.out.println("Enter category:");
+        String category = sc.next();
+        System.out.println("Enter price:");
+        double price = sc.nextDouble();
+        String resp = productCtrl.create(name, category, price);
+        System.out.println(resp);
+
+        System.out.println("0. Enter any symbol to exit...");
+        sc.next();
+    }
+
+    private void getAllProductsByCategory() {
+        System.out.println("Enter category: ");
+        String category = sc.next();
+        String resp = productCtrl.getAllByCategory(category);
+        System.out.println(resp);
+
+        System.out.println("0. Enter any symbol to exit...");
+        sc.next();
+    }
+
+    // Products END
 }
--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -12,6 +12,54 @@ public class ProductController {
         this.productRepo = productRepo;
     }
 
+    public String getById(int id) {
+        Product product = productRepo.get(id);
+
+        if (product == null)
+            return "Product with id = " + id + " does not exist.";
+
+        return product.toString();
+    }
+
+    public String getAll() {
+        List<Product> products = productRepo.getAll();
+
+        if (products.isEmpty())
+            return "Products list is empty!";
+
+        String response = "";
+        for (Product product : products)
+            response += product + "\n";
+
+        return response;
+    }
+
+    public String removeById(int id) {
+        Product product = productRepo.get(id);
+
+        if (product == null)
+            return "Product with id = " + id + " does not exist.";
+
+        boolean removed = productRepo.delete(id);
+
+        if (removed)
+            return "Product " + product.getName() + " " + product.getPrice() +
+                    "$ with category " + product.getCategory() + " was removed successfully!";
+
+        return "Product cannot be removed!";
+    }
+
+    public String create(String name, String category, double price) {
+        Product product = new Product(name, category, price);
+        boolean created = productRepo.create(product);
+
+        if (created)
+            return "A new product " + name + " " + price + "$ with category " + category
+                    + " was added successfully!";
+
+        return "Product cannot be created!";
+    }
+
     public String getAllByCategory(String category) {
         List<Product> products = productRepo.getByCategory(category);
 
